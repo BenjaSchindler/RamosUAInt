700d555f6cf0c18db01cc5bd35706b45d7aafa39
Benjamin Schindler <benja.schindler2@gmail.com>
2022-08-28 22:14:11 -0400

    cards on click expand

--- src/Routes/Blogs.tsx
+++ src/Routes/Blogs.tsx
@@ -1,32 +1,28 @@
-
 import ClassesCard from "../Components/ClassesCard";
 import { datos } from "../Components/HardCode/DataClassesCard";
+
+
 
 const Blogs: React.FC = () => {
   return (
     <main style={{ padding: "1rem 0" }}>
-      <div className="grid grid-cols-4 gap-4">
-        <h1 className="flex justify-center" >Blogs</h1>
-        
-        
+      <div className="p-1 bg-white">
         {datos?.length > 0 ? (
-          <div >
+          <div className="grid grid-cols-4 gap-2">
             {datos.map((dato) => (
-              <ClassesCard classes={dato} />
+              <div>
+                <ClassesCard classes={dato} />
+              </div>
             ))}
           </div>
         ) : (
-          <div >
+          <div>
             <h2>No datos found</h2>
           </div>
         )}
-      
-      
-      
-        </div>
+      </div>
     </main>
   );
-}
-
+};
 
 export default Blogs;
